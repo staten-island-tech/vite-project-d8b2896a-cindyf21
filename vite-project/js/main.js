8fa2089e599d5a920d2845f2ae64e812f831d732
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-11-14 10:14:12 -0500

    11/14

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,3 +2,4 @@ import '../css/style.css'
 import {menu, name} from "./menu";
 
 console.log(menu, name);
+
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -2,3 +2,109 @@ const menu = ['Rice', 'Noodles'];
 const name = "Suzy";
 
 export {menu, name};
+
+const girlgroups = [
+    {
+        group:"BLACKPINK",
+        img:"./public/blackpink.jpg",
+        price:"$64.99",
+    },
+    {
+        group:"IVE",
+        img:"./public/ive.png",
+        price:"$54.99",
+    },
+    {
+        group:"LE SSERAFIM",
+        img:"./public/LESSIRAFIM.png",
+        price:"$79.99",
+    },
+    {
+        group:"NEWJEANS ",
+        img:"./public/newjeans.png",
+        price:" $69.99",
+    },
+    {
+        group:"KEP1LER",
+        img:"./public/kep1ler.png",
+        price:"$59.99",
+    },
+    {
+        group:"AESPA",
+        img:"./public/aespa.png",
+        price:" $59.99",
+    },
+    {
+        group:"(G)I-DLE",
+        img:"./public/gilde.png",
+        price:"$59.99",
+    },
+    {
+        group:"LOONA",
+        img:"./public/loona.png",
+        price:"$64.99",
+    },
+    {
+        group:"STAYC",
+        img:"./public/stayc.png",
+        price:"$69.99",
+    },
+    {
+        group:"WEKI MEKI",
+        img:"./public/WEKI-MEKI.png",
+        price:"$55.99",
+    },
+]
+
+const boygroups = [
+    {
+        group:"SEVENTEEN",
+        img:"./public/seventeen.png",
+        price:"$69.99",
+    },
+    {
+        group:"ATEEZ",
+        img:"./public/ateez.png",
+        price:"$59.99",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+    {
+        group:"",
+        img:"",
+        price:"",
+    },
+]

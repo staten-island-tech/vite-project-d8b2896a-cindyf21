da130fd8f7a49ea4675f9256e77c2d33dd70f73a
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-11-15 10:16:55 -0500

    11/15

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -3,51 +3,62 @@ const girl = [
         group:"BLACKPINK",
         img:"./blackpink.jpg",
         price:"$64.99",
+        gen:"3rd Generation",
     },
     {
         group:"IVE",
         img:"./ive.png",
         price:"$54.99",
+        gen:"4th Generation",
+
     },
     {
         group:"LE SSERAFIM",
         img:"./LESSIRAFIM.png",
         price:"$79.99",
+        gen:"",
     },
     {
         group:"NEWJEANS ",
         img:"./newjeans.png",
         price:" $69.99",
+        gen:"",
     },
     {
         group:"KEP1LER",
         img:"./kep1ler.png",
         price:"$59.99",
+        gen:"",
     },
     {
         group:"AESPA",
         img:"./aespa.png",
         price:" $59.99",
+        gen:"",
     },
     {
         group:"(G)I-DLE",
         img:"./gilde.png",
         price:"$59.99",
+        gen:"",
     },
     {
         group:"LOONA",
         img:"./loona.png",
         price:"$64.99",
+        gen:"",
     },
     {
         group:"STAYC",
         img:"./stayc.png",
         price:"$69.99",
+        gen:"",
     },
     {
         group:"WEKI MEKI",
         img:"./WEKI-MEKI.png",
         price:"$55.99",
+        gen:"",
     },
 ]
 const boy = [
@@ -55,51 +66,61 @@ const boy = [
         group:"SEVENTEEN",
         img:"./seventeen.png",
         price:"$69.99",
+        gen:"",
     },
     {
         group:"ATEEZ",
         img:"./ateez.png",
         price:"$59.99",
+        gen:"",
     },
     {
         group:"STRAY KIDS",
         img:"./straykids.jpg",
         price:"$62.99",
+        gen:"",
     },
     {
         group:"THE BOYZ",
         img:"./theboyz.png",
         price:"$59.99",
+        gen:"",
     },
     {
         group:"ENHYPEN",
         img:"./enhypen.png",
         price:"$69.99",
+        gen:"",
     },
     {
         group:"TREASURE",
         img:"./treasure.png",
         price:"$53.99",
+        gen:"",
     },
     {
         group:"ZEROBASEONE",
         img:"./zb1.png",
         price:"$65.99",
+        gen:"",
     },
     {
         group:"GOT7",
         img:"./got7.png",
         price:"$59.99",
+        gen:"",
     },
     {
         group:"CRAVITY",
         img:"./cravity.png",
         price:"$57.99",
+        gen:"",
     },
     {
         group:"MONSTA X",
         img:"./monsta-x.png",
         price:"$59.99",
+        gen:"",
     },
 ];
 export {girl, boy};

74124bfcb3e174f8c50b6422c68c590e54f6b855
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-11-28 10:11:44 -0500

    11/28

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,5 +1,4 @@
 import "../css/style.css";
-import {menu} from "./menu.js";
 
 const DOMSelectors = {
   output: document.getElementById("output"),
@@ -10,8 +9,8 @@ const DOMSelectors = {
   thirdgen: document.getElementById("thirdgen"),
   fourthgen: document.getElementById("fourthgen"),
 };
-export{DOMSelectors};
 
+/*
 function initial() {
   menu.forEach((item) => {
     DomSelectors.output.insertAdjacentHTML(
@@ -116,20 +115,10 @@ document.getElementById("fourthgen").addEventListener("click", function() {
   clear();
   fourthgen();
 });
+*/
+
 
 /*
-function card(arr) {
-  document.querySelector(".container").insertAdjacentHTML(
-      'beforeend',
-      `<div class="gallery"> 
-      <h2 class="cardname"> ${el.group} </h2>  
-      <img class="image" src="${el.img}" alt""></img> 
-      <h3 class="price"> ${el.price} </h3> 
-      </div>`
-      )
-  }
-card(arr);
-
 DOMSelectors.girlgroup.addEventListener("click", function (event) {
   clearfields();
   const girl = kpop.filter((el) => el.bg.includes("Girl Group"))
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,6 @@
 import "../css/style.css";
 import "./dom.js";
+import {menu} from "./menu.js"
 
 document.getElementById("theme").addEventListener("click", function () {
     if (document.body.classList.contains("light")) {
@@ -11,3 +12,35 @@ document.getElementById("theme").addEventListener("click", function () {
         document.body.classList.remove("dark");
     }
 });
+
+function card(arr) {
+    arr.forEach(el => {
+      document.querySelector(".container").insertAdjacentHTML(
+        'beforeend',
+        `<div class="gallery"> 
+        <h2 class="cardname"> ${el.group} </h2>  
+        <img class="image" src="${el.img}" alt""></img> 
+        <h3 class="price"> ${el.price} </h3> 
+        </div>`
+        )
+    });
+}
+
+function clearCards() {
+    const orig = document.querySelector(".container")
+    orig.innerHTML=""
+}
+
+card(menu);
+
+function groupFilter(girlgroup) {
+    clearCards()
+    const arr = menu.filter(el => el.bg.includes(girlgroup))
+    card(arr)
+}
+
+DOMSelectors.girlgroup.addEventListener("click", function(event) {
+    event.preventDefault()
+    groupFilter("Girl Group");
+}
+)

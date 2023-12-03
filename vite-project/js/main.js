07e9c68c43908d648198a7122b91154ccb5d028b
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-12-03 17:32:07 -0500

    12/3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,6 @@
 import "../css/style.css";
-import "./dom";
+import {DOMSelectors} from "./dom.js";
+import {menu} from "./menu"
 
 document.getElementById("theme").addEventListener("click", function () {
     if (document.body.classList.contains("gray")) {
@@ -11,3 +12,150 @@ document.getElementById("theme").addEventListener("click", function () {
         document.body.classList.remove("pink");
     }
 });
+
+
+function initial() {
+  menu.forEach((item) => {
+    DOMSelectors.output.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery"> 
+        <h2 class="cardname"> ${item.group} </h2>  
+        <img class="image" src="${item.img}" alt=""></img> 
+        <h3 class="price"> ${item.price} </h3> 
+        </div>`
+    );
+  });
+}
+
+initial();
+
+function girlgroup() {
+  menu.filter((girlgroup) => girlgroup.bg.includes("Girl"))
+  .forEach((girlgroup) => {
+    DOMSelectors.output.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery"> 
+        <h2 class="cardname"> ${girlgroup.group} </h2>  
+        <img class="image" src="${girlgroup.img}" alt=""></img> 
+        <h3 class="price"> ${girlgroup.price} </h3> 
+        </div>`
+    );
+  });
+}
+
+function boygroup() {
+  menu.filter((boygroup) => boygroup.bg.includes("Boy"))
+  .forEach((boygroup) => {
+    DOMSelectors.output.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery"> 
+        <h2 class="cardname"> ${boygroup.group} </h2>  
+        <img class="image" src="${boygroup.img}" alt=""></img> 
+        <h3 class="price"> ${boygroup.price} </h3> 
+        </div>`
+    );
+  });
+}
+
+function thirdgen() {
+  menu.filter((thirdgen) => thirdgen.gen.includes("3rd"))
+  .forEach((thirdgen) => {
+    DOMSelectors.output.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery"> 
+        <h2 class="cardname"> ${thirdgen.group} </h2>  
+        <img class="image" src="${thirdgen.img}" alt=""></img> 
+        <h3 class="price"> ${thirdgen.price} </h3> 
+        </div>`
+    );
+  });
+}
+
+function fourthgen() {
+  menu.filter((fourthgen) => fourthgen.gen.includes("4th"))
+  .forEach((fourthgen) => {
+    DOMSelectors.output.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery"> 
+        <h2 class="cardname"> ${fourthgen.group} </h2>  
+        <img class="image" src="${fourthgen.img}" alt=""></img> 
+        <h3 class="price"> ${fourthgen.price} </h3> 
+        </div>`
+    );
+  });
+}
+
+function clear() {
+  DOMSelectors.output.innerHTML = "";
+}
+
+document.getElementById("reset").addEventListener("click", function() {
+  clear();
+  initial();
+});
+
+document.getElementById("girlgroup").addEventListener("click", function() {
+  clear();
+  girlgroup();
+});
+
+document.getElementById("boygroup").addEventListener("click", function() {
+  clear();
+  boygroup();
+});
+
+document.getElementById("thirdgen").addEventListener("click", function() {
+  clear();
+  thirdgen();
+});
+
+document.getElementById("fourthgen").addEventListener("click", function() {
+  clear();
+  fourthgen();
+});
+
+
+
+
+
+// function initial(arr) {
+//     arr.forEach((item) => {
+//       DOMSelectors.output.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="gallery"> 
+//           <h2 class="cardname"> ${item.group} </h2>  
+//           <img class="image" src="${item.img}" alt=""></img> 
+//           <h3 class="price"> ${item.price} </h3> 
+//           </div>`
+//       );
+//     });
+// }
+  
+// initial(menu);
+
+// DOMSelectors.filters.forEach((btn) => btn.addEventListener("click", function () {
+//     let arrays = btn.textContent;
+//     let newArr = menu.filter((menu) => fooditem.bg === arrays);
+//     initial(newArr);
+// }));
+
+
+// let buttons = document.querySelectorAll(".button")
+// buttons.forEach((button) => button.addEventListener("click", function() {
+//     let bg = button.textContent
+//     let arr = menu.filter((items) => items.bg.includes(bg));
+//     clear();
+//     initial(arr);
+// }));
+
+
+// function filters() {
+//  DOMSelectors.button.forEach((button) => button.addEventListener("click", function () {
+//     let category = button.textContent.toLowerCase()
+//     let newArr = menu.filter((menu) => menu.type.includes(category))
+//     DOMSelectors.output.innerHTML = ""
+//     initial(newArr);
+//   }))
+// }
+// filters();
+ 
